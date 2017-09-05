817c2918c143983b1fc4f6d6eeb7457f3719695b
muyizifan <839819497@qq.com>
2017-09-05 11:01:36 +0800

    配置springmvc

---
+++ babasportserviceproduct/src/main/java/cn/itcast/core/service/TestTbServiceImpl.java
@@ -0,0 +1,25 @@
+package cn.itcast.core.service;
+
+
+import cn.itcast.core.bean.TestTb;
+import cn.itcast.core.dao.TestTbDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 测试
+ * @author 1x
+ */
+@Service
+@Transactional
+public class TestTbServiceImpl implements TestTbService{
+
+    @Autowired
+    private TestTbDao testTbDao;
+
+    public void insertTestTb(TestTb testTb){
+        testTbDao.insertTestTb(testTb);
+        //throw new RuntimeException();
+    }
+}
